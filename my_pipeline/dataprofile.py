b7928d7bc6851cb6915d911a884611e67ba7f187
Shota Shirai <shotashirai@gmail.com>
2021-11-01 01:45:14 +1300

    optimize data_profile()

--- my_pipeline/dataprofile.py
+++ my_pipeline/dataprofile.py
@@ -1,63 +1,43 @@
-import math
 import pandas as pd
 
 def data_profile(df):
-    """ Data profile
     
-    Parameters
-    ----------
-    df: dataframe
-
-    Returns
-    -------
-    df_profile: dataframe for profile info
-    """
-
-    df_dtype = pd.DataFrame(df.dtypes)
+    # Create dataframe to store information
+    df_profile = pd.DataFrame(index=df.columns, columns=['Data type', 'Null count', 'Null ratio (%)', 'Non-null count', 'Distinct'])
+    
+    # Data type
+    df_profile['Data type'] = df.dtypes
+    
     # Null count
-    df_null = pd.DataFrame(df.isnull().sum())
-
-    # Null ratio (%)
-    null_ratio = df.isnull().sum() / df.shape[0] * 100 #Percentage
-    null_ratio = null_ratio.apply(lambda x: math.floor(x * 10**2) / 10**2) #rounding
-    df_null_ratio = pd.DataFrame(null_ratio)
-
+    df_profile['Null count'] = df.isna().sum()
+    
+    # Null count ratio
+    df_profile['Null ratio (%)'] = df_profile['Null count'] / len(df.index) *100
+    df_profile['Null ratio (%)'] = df_profile['Null ratio (%)'].round(2)
+    
     # Non-null count
-    df_notnull = pd.DataFrame(df.notnull().sum())
+    df_profile['Non-null count'] = df.notnull().sum()
+    
+    # Count of unique values
+    df_profile['Distinct'] = df.nunique()
 
-    # Unique value/Unique ratio(%)
-    df_unique = {}
-    df_unique_ratio = {}
-
-    for column in df.columns:
-        df_unique[column] = len(df[column].value_counts())
-        df_unique_ratio[column] = len(df[column].value_counts()) / df.shape[0] * 100 #Percentage
-        
-    df_unique = pd.DataFrame.from_dict(df_unique, orient='index')
-    df_unique_ratio = pd.DataFrame.from_dict(df_unique_ratio, orient='index')
-    df_unique_ratio = df_unique_ratio[0].apply(lambda x: math.floor(x * 10**2) / 10**2) #rounding
-
-    # Create a new dataframe showing the data profile 
-    df_profile = pd.concat([df_dtype, df_null, df_null_ratio, df_notnull, df_unique, df_unique_ratio], axis=1).reset_index()
-    df_profile.columns = ['Column', 'Data type', 'Null count', 'Null ratio (%)', 'Non-null count', 'Distinct', 'Distinct (%)']
-
+    # Number of duplicated rows
+    number_of_duplicates = df.duplicated(keep='first').sum() 
+    
     # dataframe name
-    df_name = df.name
+    try:
+        df.name
+    except AttributeError: df_name = 'Input Dataframe'
+    else: df_name = df.name
 
-    print('=== Data profile (' + df_name + ') ========================================')
+    print(''.join(['=== Data profile (', df_name, ') ========================================']))
     print(df_profile)
     print('===========================================================================')
 
-    num_dup = df.duplicated().sum()
-    if num_dup > 0:
-        print('*************************')
-        print('*** WARNING:' + str(num_dup) + 'rows are duplicated')
-        print('*************************')
-    else: 
-        print('*************************')
-        print('*** No duplicated row ')
-        print('*************************')
-
+    # number_of_duplicates = len(df)-len(df.drop_duplicates())
+    if number_of_duplicates > 0:
+        print(' '.join(['*** Warnings:', str(number_of_duplicates), 'rows are duplicated ***']))
+    else: print('*** No duplicated row ***')
 
     return df_profile
 
